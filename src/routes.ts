75d52e8fa5f8bdf8daf958ad624a5a9216cfccca
João Temponi <joao3gamerbr@gmail.com>
2025-08-08 16:47:36 -0300

    Primeiro commit

---
+++ src/routes.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import { SERVER_ROUTES } from "./appConfig.js";
+import UsuarioController from "./controller/ControllerUsuario.js";
+// import EquipamentoController from "./controller/ControllerEquipamento";
+// import Emprestimo from "./controller/ControllerEquipamento";
+// import EmprestimoController from "./controller/ControllerEmprestimo";
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ mensagem: "Rota padrão" })
+});
+
+// CRUD Usuario
+router.get(SERVER_ROUTES.LISTAR_USUARIO, UsuarioController.todos);
+router.post(SERVER_ROUTES.NOVO_USUARIO, UsuarioController.cadastrar);
+router.delete(SERVER_ROUTES.REMOVER_USUARIO, UsuarioController.remover);
+router.put(SERVER_ROUTES.ATUALIZAR_USUARIO, UsuarioController.atualizar);
+
+// router.get(SERVER_ROUTES.LISTAR_EQUIPAMENTO, EquipamentoController.todos);
+// router.post(SERVER_ROUTES.NOVO_EQUIPAMENTO, EquipamentoController.cadastrar);
+// router.delete(SERVER_ROUTES.REMOVER_EQUIPAMENTO, EquipamentoController.remover);
+// router.put(SERVER_ROUTES.ATUALIZAR_EQUIPAMENTO, EquipamentoController.atualizar);
+
+// router.get(SERVER_ROUTES.LISTAR_EMPRESTIMOS, EmprestimoController.todos);
+// router.get(SERVER_ROUTES.LISTAR_EMPRESTIMOS_DETALHADO, EmprestimoController.listarDetalhados);
+// router.post(SERVER_ROUTES.NOVO_EMPRESTIMO, EmprestimoController.cadastrar);
+// router.put(SERVER_ROUTES.ATUALIZAR_EMPRESTIMO, EmprestimoController.atualizar);
+
+export { router }
